f34878f7f9d3e1bea6ddb305a756156188980c60
Katie Humburg <pookakat@gmail.com>
2019-07-20 22:25:09 -0400

    add articles to mongodb

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,28 @@ var express = require("express");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+var db = require("../models");
+
 module.exports = function(app){
     app.get('/api/articles', (req, res, next)=>{
         axios.get("https://thehorse.com/news/").then(function(response){
             var $ = cheerio.load(response.data);
-            res.send($('.post-title').text());
+            
+            $('.post-title').each(function(i, element){
+                var result = {};
+                result.title = $(element).children("a").text();
+                result.link = $(element).children("a").attr("href");
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
+            });
         })
     });
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express'); 
-var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -7,5 +7,9 @@ var PORT = process.env.PORT || 8000;
 
 app.use("/", express.static('app/public'));
 require("./app/routing/apiRoutes.js")(app);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, function(){console.log(`Listening on port ${PORT}`)});
